1849f7d28571945b404ebf35cd819ea26f49bdef
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 01:10:48 +0200

    add machine auth api key secret

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -2,4 +2,5 @@
 echo Injecting Secrets
 export OCIS_JWT_SECRET=$(cat /run/secrets/ocis_jwt-secret)
 export OCIS_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
+export OCIS_MACHINE_AUTH_API_KEY=$(cat /run/secrets/ocis_machine-auth-api-key)
 /usr/bin/ocis server
