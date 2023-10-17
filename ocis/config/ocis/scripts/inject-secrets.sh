872930a69e47ba5b433980e4181ea03da24bebdd
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 01:04:00 +0200

    add transfer secret

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 echo Injecting Secrets
 export OCIS_JWT_SECRET=$(cat /run/secrets/ocis_jwt-secret)
+export THUMBNAIL_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
 /usr/bin/ocis server
