12b94338cec79a0921633228384e031722e0eb2b
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 01:08:39 +0200

    fix var name

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Injecting Secrets
 export OCIS_JWT_SECRET=$(cat /run/secrets/ocis_jwt-secret)
-export THUMBNAIL_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
+export OCIS_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
 /usr/bin/ocis server
