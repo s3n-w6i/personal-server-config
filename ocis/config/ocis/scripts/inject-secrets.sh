537a9b2f3278d6f5c641a9d5b61ff8dbbd1e007e
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 12:05:10 +0200

    add admin password secret

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -5,4 +5,5 @@ export OCIS_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
 export OCIS_MACHINE_AUTH_API_KEY=$(cat /run/secrets/ocis_machine-auth-api-key)
 export OCIS_SYSTEM_USER_ID=$(cat /run/secrets/ocis_storage-system-user-id)
 export OCIS_SYSTEM_USER_API_KEY=$(cat /run/secrets/ocis_storage-system-api-key)
+export IDM_ADMIN_PASSWORD=$(cat /run/secrets/ocis_idm-admin-password)
 /usr/bin/ocis server
