57d345b1064b308a8f56ac3bfedf31f7ae307bd5
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 11:54:21 +0200

    inject storage system secrets

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -3,4 +3,6 @@ echo Injecting Secrets
 export OCIS_JWT_SECRET=$(cat /run/secrets/ocis_jwt-secret)
 export OCIS_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
 export OCIS_MACHINE_AUTH_API_KEY=$(cat /run/secrets/ocis_machine-auth-api-key)
+export OCIS_SYSTEM_USER_ID=$(cat /run/secrets/ocis_storage-system-user-id)
+export OCIS_SYSTEM_USER_API_KEY=$(cat /run/secrets/ocis_storage-system-api-key)
 /usr/bin/ocis server
