3234c2d1188f4a4306f524b67763438c85f74dbb
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 12:40:15 +0200

    set admin password from variable

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo Injecting Secrets
 export IDM_ADMIN_PASSWORD=$(cat /run/secrets/ocis_idm-admin-password)
-/usr/bin/ocis init || true
+/usr/bin/ocis init --admin-password ${IDM_ADMIN_PASSWORD} || true
 /usr/bin/ocis server
