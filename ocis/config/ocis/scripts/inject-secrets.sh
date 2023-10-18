28f0b75e7d84ff014aab0c89fdb679688467d56d
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 12:44:37 +0200

    inject admin password the other way

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo Injecting Secrets
-export IDM_ADMIN_PASSWORD=$(cat /run/secrets/ocis_idm-admin-password)
-/usr/bin/ocis init --admin-password ${IDM_ADMIN_PASSWORD} || true
+echo Init
+IDM_ADMIN_PASSWORD=$(cat /run/secrets/ocis_idm-admin-password) && /usr/bin/ocis init --admin-password ${IDM_ADMIN_PASSWORD} || true
+echo Starting server
 /usr/bin/ocis server
