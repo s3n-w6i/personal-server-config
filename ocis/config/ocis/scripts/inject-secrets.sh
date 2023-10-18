a307178d45548913e79468b6bd9850f2f5804fb4
st183888 <37022952+pr0gr8mm3r@users.noreply.github.com>
2023-10-18 12:24:25 +0200

    run ocis init

--- ocis/config/ocis/scripts/inject-secrets.sh
+++ ocis/config/ocis/scripts/inject-secrets.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 echo Injecting Secrets
-export OCIS_JWT_SECRET=$(cat /run/secrets/ocis_jwt-secret)
-export OCIS_TRANSFER_SECRET=$(cat /run/secrets/ocis_transfer-secret)
-export OCIS_MACHINE_AUTH_API_KEY=$(cat /run/secrets/ocis_machine-auth-api-key)
-export OCIS_SYSTEM_USER_ID=$(cat /run/secrets/ocis_storage-system-user-id)
-export OCIS_SYSTEM_USER_API_KEY=$(cat /run/secrets/ocis_storage-system-api-key)
 export IDM_ADMIN_PASSWORD=$(cat /run/secrets/ocis_idm-admin-password)
+/usr/bin/ocis init
 /usr/bin/ocis server
